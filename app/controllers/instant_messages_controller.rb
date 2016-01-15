051737f7797caaed310406e43e9da78df75bbef5
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-15 17:47:53 +0000

    Add new action and view

--- app/controllers/instant_messages_controller.rb
+++ app/controllers/instant_messages_controller.rb
@@ -3,4 +3,11 @@ class InstantMessagesController < ApplicationController
   def index
     @instant_messages = InstantMessage.all
   end
+  
+  def new
+    @instant_message = InstantMessage.new
+  end
+  
+  def create
+  end
 end
