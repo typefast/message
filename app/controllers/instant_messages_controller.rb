2515e42b7f1dcff4ed966e2760e6e5b10118c82e
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-15 17:52:15 +0000

    Add create action

--- app/controllers/instant_messages_controller.rb
+++ app/controllers/instant_messages_controller.rb
@@ -8,6 +8,22 @@ class InstantMessagesController < ApplicationController
     @instant_message = InstantMessage.new
   end
   
-  def create
+  def show
   end
+  
+  def create
+    @instant_message = InstantMessage.create(instant_message_params)
+    if @instant_message.save
+      flash[:success] = "New message created!"
+      redirect_to @instant_message
+    else
+      flash[:error] = "There was an error!"
+      render :new
+    end
+  end
+  
+  private 
+    def instant_message_params
+      params.require(:instant_message).permit(:message)
+    end
 end
